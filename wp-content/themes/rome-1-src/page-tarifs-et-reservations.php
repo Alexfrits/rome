d49a83b036ca8218242031ccd251ed291d35591e
Mehdi M <meduzen@gmail.com>
2015-04-16 16:24:43 +0200

    DB: Tarifs et réservations (FR content) and ACF Maps (required). Pricing: .php file created.

---
+++ wp-content/themes/rome-1-src/page-tarifs-et-reservations.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<?php now_in(__FILE__) ?>
+
+<p><strong>TODO : choper le contenu de la page avec la WP-loop, puis ajouter le formulaire.</strong></p>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>

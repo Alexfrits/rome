dee8098650b197037922646ba40380bf221a1bea
Alexandre Frits <frits.alex@gmail.com>
2015-04-24 00:47:01 +0200

    started styling archive-guides_php

--- wp-content/themes/rome-1-src/archive-guides.php
+++ wp-content/themes/rome-1-src/archive-guides.php
@@ -14,10 +14,14 @@
       $ville = get_field_object('ville');
       $photoUrl = get_field_object('photo')['value']['sizes']['carre']; ?>
       <li class="guides-list__item">
-        <?php the_title(); ?>
+      <div class="guides-list__item__col--left">
+        <h2 class="guides-list__item__nom"><?php the_title(); ?></h2>
         <img class="guides-list__item__img" src="<?php echo $photoUrl; ?>" alt="">
+      </div>
+      <div class="guides-list__item__col--right">
         <p class="guides-list__item__formation"><?php echo $formation['value']; ?></p>
         <p class="guides-list__item__ville"><?php echo $ville['value']; ?></p>
+      </div>
       </li>
     <?php endwhile; ?>
     </ul>
